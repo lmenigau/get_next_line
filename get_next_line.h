ebbd9561a9d6afec2e9226c0c22ab3ad7d1284b3
Louis MENIGAULT <lmenigau@e2r6p6.42.fr>
2016-12-12 19:40:34 +0100

    header dir

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: lmenigau <lmenigau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/06 13:19:39 by lmenigau          #+#    #+#             */
-/*   Updated: 2016/12/10 11:09:19 by lmenigau         ###   ########.fr       */
+/*   Updated: 2016/12/12 19:38:38 by lmenigau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "libft.h"
+# include "libft/libft.h"
 # define BUFF_SIZE	4096
 
 typedef struct		s_file
